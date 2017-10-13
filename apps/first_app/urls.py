35c9f147464bb46b085d1225388961552a32e004
ReubenBagtas <ryan_deleon4x4@yahoo.com>
2017-10-12 21:19:21 -0700

    add urls

--- apps/first_app/urls.py
+++ apps/first_app/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^about$', views.about),
+    url(r'^packages$', views.packages),
+    url(r'^contact$', views.contact),
+    url(r'^testimonials$', views.testimonials),
+
 ]
--- apps/first_app/views.py
+++ apps/first_app/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -12,3 +12,17 @@ def index(request):
         "name": "mike"
     }
     return render(request, "index.html", context)
+
+def about(request):
+    return render(request, "about.html")
+
+def packages(request):
+    return render(request, "packages.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def testimonials(request):
+    return render(request, "testimonials.html")
+
+
